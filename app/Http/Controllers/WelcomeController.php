6be33c70ca8ac26b6f434c6a08d56e2311ea1ea1
esmail <87426295+Jobayer53@users.noreply.github.com>
2024-09-23 17:50:53 +0600

    wizard validate

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class WelcomeController extends Controller
 {
@@ -11,6 +12,20 @@ class WelcomeController extends Controller
     }
 
     public function welcome_store(Request $request){
-        dd($request->all());
+
+        $validate = Validator::make($request->all(), [
+            'web_name' => 'required',
+            'domain' => 'required',
+            'email' => 'required',
+            'password' => 'required',
+        ],[
+            'domain' => 'Domain is required',
+            'web_name'  => 'Web Name is required',
+            'email' => 'Email is required',
+            'password' => 'Password is required',
+        ]);
+        if($validate->fails()){
+            return redirect()->back()->withErrors($validate)->withInput();
+        }
     }
 }
