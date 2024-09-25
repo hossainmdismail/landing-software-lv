4e44a47f8209131a6d752814b13b8b10ff8ac565
Jobayer Shikder <mdjobayer.h53@gmail.com>
2024-09-25 12:11:22 +0600

    setup

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/',[WelcomeController::class, 'welcome'])->name('welcome');
 Route::post('/welcome/store',[WelcomeController::class, 'welcome_store'])->name('welcome.store');
+Route::
